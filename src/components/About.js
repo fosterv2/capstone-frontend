3f1c967c99be063a51c11c935968bac9727f56d4
github username <github email address>
2020-06-17 11:58:18 -0700

    added a logo

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,8 @@ const About = () => {
             <p>
                 An app for cats to connect with each other and share their thoughts and feelings
                 (because cats do have feelings).
+            </p>
+            <p>
                 A user can see all posts, logged in or not.
                 Once they have logged in they can make a post with a picture and content.
                 They can also make comments or add likes on any posts.
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -3,10 +3,18 @@ import '../css/Profile.css'
 
 const Profile = ({ user, history }) => {
     const { username, breed, owner_name, img_url } = user
+
+    const isPicture = () => {
+        if (img_url) {
+            const format = img_url.split(".").pop()
+            return format === "gif" || format === "jpg" || format === "png"
+        }
+    }
+
     return (
         <div className="profile card">
             <h2>{username}</h2>
-            <img src={img_url} alt="profile" />
+            <img src={isPicture() ? img_url : "https://i.imgur.com/VFDasqc.png"} alt="profile" />
             <div className="info">
                 <p>Breed: {breed}</p>
                 <p>Owner: {owner_name}</p>
