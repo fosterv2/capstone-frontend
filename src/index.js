2e0893f3fefa6acf00f516b075dbdb316471f51c
fosterv2 <fosterv2@outlook.com>
2020-09-01 16:30:56 -0700

    install and add action-cable-provider

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 import rootReducer from './redux/rootReducer'
+import { ActionCableProvider } from 'react-actioncable-provider'
+import AC_BASE_URL from './redux'
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ActionCableProvider url={AC_BASE_URL}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ActionCableProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,5 +1,7 @@
 export * from './post/postActions'
 export * from './group/groupActions'
 export * from './user/userActions'
-export const BASE_URL = "https://cat-space-backend.herokuapp.com/"
-// export const BASE_URL = "http://localhost:3000/"
+// export const BASE_URL = "https://cat-space-backend.herokuapp.com/"
+// export const AC_BASE_URL = "ws://cat-space-backend.herokuapp.com/cable"
+export const BASE_URL = "http://localhost:3000/"
+export const AC_BASE_URL = "ws://localhost:3000/cable"
